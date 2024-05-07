bdf2b9ba62551ef7019754324e6675b115009bb6
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2024-05-07 18:33:58 -0400

    Add multiple accordion items

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,24 @@ export default function App() {
     return (
         <main>
             <section>
-                <h2>Why work for us?</h2>
+                <h2>Get back down now.</h2>
                 <Accordion className={"accordion"}>
-                    <AccordionItem title="Item 1">
+                    <AccordionItem title="Item 1" className="accordion-item">
                         <article>
                             <p>This is an item that has a lot of words.</p>
                             <p>Billy answered: "It only gets easier."</p>
+                        </article>
+                    </AccordionItem>
+                    <AccordionItem title="Item 2" className="accordion-item">
+                        <article>
+                            <p>I'm the one that needs to find out.</p>
+                            <p>Bitter sister asked mister.</p>
+                        </article>
+                    </AccordionItem>
+                    <AccordionItem title="Item 3" className="accordion-item">
+                        <article>
+                            <p>I'm the one that craves the last match.</p>
+                            <p>"Why do you think it's so unbearable?"</p>
                         </article>
                     </AccordionItem>
                 </Accordion>
