7501c55e303283d26f0fa2e409cf8bd87b9222d4
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2024-05-08 17:08:17 -0400

    Add click listener

--- src/components/AccordionItem.jsx
+++ src/components/AccordionItem.jsx
@@ -1,16 +1,15 @@
 import { useAccordionContext } from "./Accordion";
 
-export default function AccordionItem({
-    parentId,
-    className,
-    title,
-    children,
-}) {
+export default function AccordionItem({ id, className, title, children }) {
     const accordionContext = useAccordionContext();
-    const isOpen = parentId === accordionContext.openItemId;
+    const isOpen = id === accordionContext.openItemId;
+
+    const clickHandler = () => {
+        accordionContext.openItem(id);
+    };
 
     return (
-        <li>
+        <li className={className} onClick={clickHandler}>
             <h3>{title}</h3>
             <div className={isOpen ? "open" : undefined}>{children}</div>
         </li>
