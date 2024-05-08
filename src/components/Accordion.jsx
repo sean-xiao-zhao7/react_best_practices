2ac6bcd80c5f0d7b721a72b810f277224fdee82b
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2024-05-08 16:06:05 -0400

    Add AccordionContext

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -1,7 +1,27 @@
+import { createContext, useState } from "react";
+
+const AccordionContext = createContext();
+
 export default function Accordion({ children, className }) {
+    const [openItemId, setOpenItemId] = useState(null);
+
+    const openItem = (itemId) => {
+        setOpenItemId(itemId);
+    };
+
+    const closeItems = () => {
+        setOpenItemId(null);
+    };
+
+    const contextState = {
+        openItemId,
+        openItem,
+        closeItems,
+    };
+
     return (
-        <>
+        <AccordionContext.Provider value={contextState}>
             <ul className={className}>{children}</ul>
-        </>
+        </AccordionContext.Provider>
     );
 }
