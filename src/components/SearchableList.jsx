8b6adc794e9f90d9d744e0f87bcd2d008d9d2983
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2024-05-13 19:09:06 -0400

    Add searchableList logic

--- src/components/SearchableList.jsx
+++ src/components/SearchableList.jsx
@@ -1,1 +1,12 @@
-export default function SearchableList() {}
+export default function SearchableList({ listItems }) {
+    return (
+        <div className="searchable-list">
+            <input type="search" placeholder="Enter filter words" />
+            <ul>
+                {listItems.map((item) => {
+                    return <li key={item}>{item}</li>;
+                })}
+            </ul>
+        </div>
+    );
+}
