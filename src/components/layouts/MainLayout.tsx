77a8bc5379cd5a8cd2583643a81c5094186041ba
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2024-05-24 15:10:11 -0400

    Add menu

--- src/components/layouts/MainLayout.tsx
+++ src/components/layouts/MainLayout.tsx
@@ -1,9 +1,14 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 
 export default function MainLayout() {
     return (
         <main>
+            <menu>
+                <Link to="/">Home</Link>
+                <Link to="/accordion">Accordion Test</Link>
+                <Link to="/searchablelist">Searchable List Test</Link>
+            </menu>
             <section>
                 <Outlet />
             </section>
