3324e03a9132a77d08049bd7f7e2aaa127edbf40
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2024-05-30 12:30:13 -0400

    Display calculator results properly

--- src/components/investment-calculator/CalulatorResults.tsx
+++ src/components/investment-calculator/CalulatorResults.tsx
@@ -7,17 +7,28 @@ export default function CalculatorResults({
     results: InvestmentResultType[];
 }) {
     return (
-        <div id="investment-calculator-results">
+        <>
+            <div id="investment-calculator-results-headings">
+                <div></div>
+                <div>Annual Investment</div>
+                <div>Interest</div>
+                <div>Value at End of Year</div>
+                <div>Year</div>
+            </div>
             {results.map((result) => {
                 return (
-                    <div key={result.id}>
-                        {result.annualInvestment}
-                        {result.interest}
-                        {result.valueEndOfYear}
-                        {result.year}
+                    <div
+                        key={result.id}
+                        className="investment-calculator-results-row"
+                    >
+                        Entry:
+                        <div>{result.annualInvestment}</div>
+                        <div>{result.interest}</div>
+                        <div>{result.valueEndOfYear}</div>
+                        <div>{result.year}</div>
                     </div>
                 );
             })}
-        </div>
+        </>
     );
 }
