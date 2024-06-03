f1b3ed699fc1146a0cd29aa4c8211fe41f2e7472
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2024-06-03 17:21:55 -0400

    Add modal comp

---
+++ src/components/modal/Modal.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Modal() {
+    return (
+        <>
+            <dialog>
+                <h2>Modal title</h2>
+                <p>Modal body.</p>
+            </dialog>
+        </>
+    );
+}
